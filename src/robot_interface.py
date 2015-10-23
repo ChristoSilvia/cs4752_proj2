312d17e4305a60025adc0d9d353a26683f317bdb
ChristoSilvia <christopher.silvia@gmail.com>
2015-10-23 10:03:07 -0400

    Added velocity control

--- src/robot_interface.py
+++ src/robot_interface.py
@@ -7,12 +7,13 @@ from scipy.spatial import KDTree
 import rospy
 from cs4752_proj2.srv import *
 import baxter_interface
+from baxter_interface import CHECK_VERSION
 from baxter_pykdl import baxter_kinematics
 
 
 
 # FIXED PARAMETERS
-joint_names = ['S0','S1','E0','E1','W0','W1','W2']
+joint_names = ['left_s0','left_s1','left_e0','left_e1','left_w0','left_w1','left_w2']
 
 joint_limits = np.array([[-2.461, 0.890],
                          [-2.147, 1.047],
@@ -50,10 +51,39 @@ def nearest_neighbor_response(req):
     distance, nearest_point_index = kdtree.query(np.array(req.point))
     return NearestNeighborResponse(kdtree.data[nearest_point_index,:])
 
+def get_joint_velocities(workspace_velocity):
+    global left_kin
+    jacobian_pinv = left_kin.jacobian_pseudo_inverse()
+    return np.dot(jacobian_pinv, workspace_velocity)
 
+def moveto(final_position, speed):
+    global left
+    loginfo("Recieved Move Order")
+    dt = 0.05
+    velocities = {}
+    current_position = np.array(left.endpoint_pose()['position'])
+    loginfo("Current Position: {0}".format(current_position))
+    total_distance = np.linalg.norm(current_position - final_position)
+    loginfo("Distance to Target: {0}".format(total_distance))
+    total_time = total_distance / speed
+    loginfo("Expected Time at Desired Speed: {0}".format(total_time))
+    velocity = speed * (final_position - current_position) / total_distance
+    loginfo("Expected End Effector Velocity")
+    left.set_command_timeout(3*dt)
+    for t in np.arange(0, total_time, dt):
+        t_start = rospy.get_time()
+        velocity_and_angular_momentum = np.zeros((6,1))
+        velocity_and_angular_momentum[0:3,0] = velocity
+        joint_velocities = get_joint_velocities(velocity_and_angular_momentum)
+        for i, name in enumerate(joint_names):
+            velocities[name] = joint_velocities[i,0]
+        left.set_joint_velocities(velocities)
+        rospy.sleep(dt - (rospy.get_time() - t_start))
 
 def robot_interface():
     global kdtree
+    global left
+    global left_kin
     rospy.init_node('robot_interface')
     loginfo("Initialized Robot Interface")
 
@@ -71,8 +101,35 @@ def robot_interface():
     nearest = rospy.Service('nearest', NearestNeighbor, nearest_neighbor_response)
     loginfo("Initialized Nearest Neighbor Service")
 
+    baxter_interface.RobotEnable(CHECK_VERSION).enable()
+
     left = baxter_interface.Limb('left')
     left_kin = baxter_kinematics('left')
+#    velocities = {}
+#    zero_velocity = {}
+#    for joint_name in joint_names:
+#        zero_velocity[joint_name] = 0.0
+#    loginfo("Starting to move arm")
+#    left.set_command_timeout(0.5)
+##    for i in xrange(0,200):
+##        c_space_velocities = get_joint_velocities(np.array([[0],[0],[0.01],[0],[0],[0]]))
+##        for j, joint_name in enumerate(joint_names):
+##            velocities[joint_name] = c_space_velocities[j,0]
+##        left.set_joint_velocities(velocities)
+##        rospy.sleep(0.1)
+#    for i in xrange(0,200):
+#        c_space_velocities = get_joint_velocities(np.array([[0],[0],[0.0],[0.1],[0.0],[0]]))
+#        for j, joint_name in enumerate(joint_names):
+#            velocities[joint_name] = c_space_velocities[j,0]
+#        left.set_joint_velocities(velocities)
+#        rospy.sleep(0.1)
+#    left.exit_control_mode()
+
+    current_pose = np.array(left.endpoint_pose()['position'])
+    loginfo("Current pose: {0}".format(current_pose))
+    desired_pose = current_pose + np.array([0,0,0.2])
+    loginfo("Desired Post: {0}".format(desired_pose))
+    moveto(desired_pose, 0.05)
 
     rospy.spin()
 
