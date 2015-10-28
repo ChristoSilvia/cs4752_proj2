15f62fd7af021008e8451e7a303955e6fb123cf3
ChristoSilvia <christopher.silvia@gmail.com>
2015-10-27 22:45:20 -0400

    Added tune code

--- src/joint_action_server.py
+++ src/joint_action_server.py
@@ -4,6 +4,8 @@ import numpy as np
 from scipy.spatial import KDTree
 from scipy.interpolate import PiecewisePolynomial
 
+from matplotlib import pyplot as plt
+
 import rospy
 from geometry_msgs.msg import Vector3
 import baxter_interface
@@ -19,8 +21,9 @@ class JointActionServer():
         self.limb = baxter_interface.Limb(limb_name)
         self.limb_kin = baxter_kinematics(limb_name)
         self.joint_names = self.limb.joint_names()
-        self.kp = 0.004
-        self.dt = 0.005
+        self.kp = 0.05
+        self.ki = 0.01
+        self.dt = 0.01
 
         self.joint_action_srv = rospy.Service('joint_action', JointAction, self.move_trajectory)
         self.position_srv = rospy.Service('end_effector_position', EndEffectorPosition, self.get_position_response)
@@ -30,35 +33,65 @@ class JointActionServer():
 
     def move_trajectory(self, args):
         times, x_positions_velocities, y_positions_velocities, z_positions_velocities = self.unpack_joint_action_message(args)
-        
+        loginfo("Times: {0}".format(times))
+        loginfo("x Positions/Velocities: {0}".format(x_positions_velocities)) 
+        loginfo("y Positions/Velocities: {0}".format(x_positions_velocities)) 
+        loginfo("z Positions/Velocities: {0}".format(x_positions_velocities)) 
+
+ 
         # add in derivatices
         spline_order = 3
         xinterpolator = PiecewisePolynomial(times, x_positions_velocities, orders=spline_order, direction=1)
         yinterpolator = PiecewisePolynomial(times, y_positions_velocities, orders=spline_order, direction=1)
         zinterpolator = PiecewisePolynomial(times, z_positions_velocities, orders=spline_order, direction=1)
         
-        T = np.arange(0, times[-1], dt)
-        n = length(T)
+        T = np.arange(0, times[-1], self.dt)
+        n = len(T)
         velocity_and_w = np.zeros((6,1))
-        velocity_and_w[0] = xinterpolator.derivative(times[0])
-        velocity_and_w[1] = yinterpolator.derivative(times[0])
-        velocity_and_w[2] = zinterpolator.derivative(times[0])
+        velocity_and_w[0] = xinterpolator.derivative(T[0])
+        velocity_and_w[1] = yinterpolator.derivative(T[0])
+        velocity_and_w[2] = zinterpolator.derivative(T[0])
+        vxs, vys, vzs = np.empty(n), np.empty(n), np.empty(n)
+        vx_corrector, vy_corrector, vz_corrector = 0.0, 0.0, 0.0
+        vx_integral, vy_integral, vz_integral = 0.0, 0.0, 0.0
+        vxs[0] = vx_corrector
+        vys[0] = vy_corrector
+        vzs[0] = vz_corrector
         for i in xrange(1,n):
             t_start = rospy.get_time()
             self.limb.set_joint_velocities(
                 self.make_velocity_dict(
                     self.get_joint_velocities(velocity_and_w)))
             
-            velocity_and_w[0] = xinterpolator.derivative(times[i])
-            velocity_and_w[1] = yinterpolator.derivative(times[i])
-            velocity_and_w[2] = zinterpolator.derivative(times[i])
+            position = self.get_position()
+            loginfo(position)
+            loginfo("{0},{1},{2}".format(xinterpolator(T[i]),
+                                         yinterpolator(T[i]),
+                                         zinterpolator(T[i])))
+            vx_corrector = xinterpolator(T[i]) - position[0]
+            vy_corrector = yinterpolator(T[i]) - position[1]
+            vz_corrector = zinterpolator(T[i]) - position[2]
+            vx_integral += vx_corrector * (T[i] - T[i-1])
+            vy_integral += vy_corrector * (T[i] - T[i-1])
+            vz_integral += vz_corrector * (T[i] - T[i-1])
+            vxs[i] = vx_corrector
+            vys[i] = vy_corrector
+            vzs[i] = vz_corrector
+
+            velocity_and_w[0] = xinterpolator.derivative(T[i]) + self.kp * vx_corrector + self.ki * vx_integral
+            velocity_and_w[1] = yinterpolator.derivative(T[i]) + self.kp * vy_corrector + self.ki * vy_integral
+            velocity_and_w[2] = zinterpolator.derivative(T[i]) + self.kp * vz_corrector + self.ki * vz_integral
                      
-    
             desired_interval = T[i] - T[i-1]
             end_time = T[i] - T[i-1] + t_start
-            rospy.sleep(end_time - rospy.get_time()) 
+            rospy.sleep(end_time - rospy.get_time())
    
         self.limb.exit_control_mode()     
+        plt.plot(T, vxs)
+        plt.plot(T, vys)
+        plt.plot(T, vzs)
+        plt.show()
+
         return JointActionResponse()
 
     def get_joint_velocities(self, workspace_velocity_and_w):
@@ -78,36 +111,34 @@ class JointActionServer():
         times_array[0] = 0.0
         times_array[1:] = args.times
         
-	x_positions_velocities = np.empty((len(args.positions)+1, 2))
-	y_positions_velocities = np.empty((len(args.positions)+1, 2))
-	z_positions_velocities = np.empty((len(args.positions)+1, 2))
+        x_positions_velocities = np.empty((n+1, 2))
+        y_positions_velocities = np.empty((n+1, 2))
+        z_positions_velocities = np.empty((n+1, 2))
 
-	for i in xrange(0,n):
-            x_positions_velocities[i,0] = args.positions[i].x
-            y_positions_velocities[i,0] = args.positions[i].y
-            z_positions_velocities[i,0] = args.positions[i].z
+        for i in xrange(0,n):
+            x_positions_velocities[i+1,0] = args.positions[i].x
+            y_positions_velocities[i+1,0] = args.positions[i].y
+            z_positions_velocities[i+1,0] = args.positions[i].z
 
-            x_positions_velocities[i,1] = args.velocities[i].x
-            y_positions_velocities[i,1] = args.velocities[i].y
-            z_positions_velocities[i,1] = args.velocities[i].z
+            x_positions_velocities[i+1,1] = args.velocities[i].x
+            y_positions_velocities[i+1,1] = args.velocities[i].y
+            z_positions_velocities[i+1,1] = args.velocities[i].z
 
-	current_velocity = self.get_velocity()
-        x_positions_velocities[0,0] = current_velocity[0]
-        y_positions_velocities[0,0] = current_velocity[1]
-        z_positions_velocities[0,0] = current_velocity[2]
+	    current_velocity = self.get_velocity()
+        x_positions_velocities[0,1] = current_velocity[0]
+        y_positions_velocities[0,1] = current_velocity[1]
+        z_positions_velocities[0,1] = current_velocity[2]
         
         current_position = self.get_position()
-        x_positions_velocities[0,1] = current_position[0]
-        y_positions_velocities[0,1] = current_position[1]
-        z_positions_velocities[0,1] = current_position[2]
+        x_positions_velocities[0,0] = current_position[0]
+        y_positions_velocities[0,0] = current_position[1]
+        z_positions_velocities[0,0] = current_position[2]
 
         return times_array, x_positions_velocities, y_positions_velocities, z_positions_velocities
 
     def get_position_response(self, args):
-        loginfo("Recieved position request")
-        loginfo("Arguments: {0}".format(args)) 
         position = self.get_position()
-        return EndEffectorPositionResponse(Vector3(positon[0],position[1],position[2]))
+        return EndEffectorPositionResponse(Vector3(position[0],position[1],position[2]))
 
     def get_velocity_response(self, args):
         velocity = self.get_velocity()
--- src/test.py
+++ src/test.py
@@ -3,6 +3,7 @@ import numpy as np
 from scipy.spatial import KDTree
 from scipy.interpolate import PiecewisePolynomial
 
+
 import rospy
 from geometry_msgs.msg import Vector3
 from cs4752_proj2.srv import *
@@ -23,15 +24,35 @@ def test():
     joint_action_server = rospy.ServiceProxy("/joint_action", JointAction)
     loginfo("Initialized Joint Action Server Proxy")
     rospy.wait_for_service("/end_effector_position")
-    position_server = rospy.ServiceProxy("/end_effector_position", JointAction)
+    position_server = rospy.ServiceProxy("/end_effector_position", EndEffectorPosition)
     loginfo("Initialized position server proxy")
 
+#    rospy.sleep(5.0)
+
     loginfo("Making position call")
-    current_position = position_server()
-    loginfo(current_position)
-    joint_action_server([1.0], 
-                        [Vector3(current_position.x, current_position.y + 0.1, current_position.z + 0.1)],    
-                        [Vector3(0.0,0.0,0.0)])
+    initial_position = position_server().position
+    loginfo(initial_position)
+    joint_action_server([5.0, 10.0],
+                        [Vector3(initial_position.x + 0.1*np.random.rand(),
+                                 initial_position.y + 0.1*np.random.rand(),
+                                 initial_position.z + 0.1*np.random.rand()),
+                         initial_position],
+                        [Vector3(0.0, 0.0, 0.0),
+                         Vector3(0.0, 0.0, 0.0)])
+    loginfo(initial_position)
+    loginfo(position_server().position)
+    loginfo(position_server().position.x - initial_position.x)      
+    loginfo(position_server().position.y - initial_position.y)      
+    loginfo(position_server().position.z - initial_position.z)      
+     
+#       joint_action_server([4.0, 8.0], 
+#                           [Vector3(current_position.x+0.05, 
+#                                    current_position.y-0.05, 
+#                                    current_position.z-0.005),
+#                            Vector3(current_position.x,
+#                                    current_position.y-0.1,
+#                                    current_position.z-0.01)],    
+#                           [Vector3(0.0,-0.05,0.0), Vector3(0.05,0.0,0.0)])
 
 if __name__ == '__main__':
     test()
