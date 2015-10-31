a269b0cf2695bdaa0065c66515bca953d9d40a8e
ChristoSilvia <christopher.silvia@gmail.com>
2015-10-31 17:48:02 -0400

    working on getting plane_traj set up like example circle

--- src/test-bezier.py
+++ src/test-bezier.py
@@ -5,8 +5,9 @@ from scipy.interpolate import PiecewisePolynomial
 
 
 import rospy
-from geometry_msgs.msg import Vector3
+from cs4752_proj2.msg import *
 from cs4752_proj2.srv import *
+from geometry_msgs.msg import Vector3
 import baxter_interface
 from baxter_interface import CHECK_VERSION
 from baxter_pykdl import baxter_kinematics
@@ -17,9 +18,11 @@ def loginfo(logstring):
     rospy.loginfo("Controller: {0}".format(logstring))
 
 def test():
+    global position_server
     rospy.init_node('test')
     loginfo("Initialized node Controller")
 
+    global joint_action_server
     rospy.wait_for_service("/move_end_effector_trajectory")
     joint_action_server = rospy.ServiceProxy("/move_end_effector_trajectory", JointAction)
     tool_trajectory = rospy.ServiceProxy("/move_tool_trajectory", JointAction)
@@ -28,11 +31,32 @@ def test():
     position_server = rospy.ServiceProxy("/end_effector_position", EndEffectorPosition)
     loginfo("Initialized position server proxy")
 
+    rospy.Subscriber("/plane_traj", Trajectory, plane_trajCb, queue_size=10000)
 #    rospy.sleep(5.0)
 
+    rospy.spin()
+
+def plane_trajCb(plane_traj_msg):
+    global position_server
+    global joint_action_server
     loginfo("Making position call")
     initial_position = position_server().position
     loginfo(initial_position)
+
+    print "#################################"
+    print "plane_traj_msg"
+    print "len(times): %d" % len(plane_traj_msg.times)
+    print "#################################"
+
+    T = np.array(plane_traj_msg.times)
+    # for i in range(0,len(plane_traj_msg.times)):
+
+
+# float64[] times
+# geometry_msgs/Vector3[] positions
+# geometry_msgs/Vector3[] velocities
+
+
 
     A = 0.06
 
@@ -69,14 +93,14 @@ def test():
     loginfo(position_server().position.y - initial_position.y)      
     loginfo(position_server().position.z - initial_position.z)      
      
-#       joint_action_server([4.0, 8.0], 
-#                           [Vector3(current_position.x+0.05, 
-#                                    current_position.y-0.05, 
-#                                    current_position.z-0.005),
-#                            Vector3(current_position.x,
-#                                    current_position.y-0.1,
-#                                    current_position.z-0.01)],    
-#                           [Vector3(0.0,-0.05,0.0), Vector3(0.05,0.0,0.0)])
+    # joint_action_server([4.0, 8.0], 
+    #     [Vector3(current_position.x+0.05, 
+    #            current_position.y-0.05, 
+    #            current_position.z-0.005),
+    #     Vector3(current_position.x,
+    #            current_position.y-0.1,
+    #            current_position.z-0.01)],    
+    #               [Vector3(0.0,-0.05,0.0), Vector3(0.05,0.0,0.0)])
 
 if __name__ == '__main__':
     test()
