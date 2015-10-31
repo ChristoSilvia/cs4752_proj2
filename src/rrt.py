3eaa5089d5ea021a9f4a3c91c4eb056287a11d72
CS4752 <cs4752@en-cs-lgg03-04.coecis.cornell.edu>
2015-10-31 16:45:49 -0400

    rrt kind of working, needs instructor collision test and patience(while its running)

--- src/rrt.py
+++ src/rrt.py
@@ -8,7 +8,8 @@ import baxter_interface
 from baxter_interface import CHECK_VERSION
 from baxter_pykdl import baxter_kinematics
 from tf.transformations import *
-
+from copy import deepcopy
+import random
 
 
 
@@ -26,8 +27,13 @@ def sample_cspace():
 	r = np.zeros(7)
 	for i in xrange(0,7) :
 		r[i] = temp[i]
+	return r#sample_3d()
+def sample_3d() :
+	r = np.zeros(3)
+	r[0] = np.random.rand(1)*10
+	r[1] = np.random.rand(1)*10
+	r[2] = np.random.rand(1)*10
 	return r
-
 
 #use the course staff collision checks here
 def Point_Collision_Check(point) :
@@ -49,19 +55,27 @@ class Buffered_KD_Tree :
 		#self.kdtree = KDTree(self.kdtree_data)
 
 	def insert(self, p) :
-		print "INSERTING"
+		#print "INSERTING"
+		#print p.shape
+		#print self.buffer_count
 		if self.buffer_count == self.buffer_size :
 			
-			new_to_tree = np.hstack(self.buffer)
+			new_to_tree = np.vstack(self.buffer)
+			#print "ADDING TO KD TREE"
+			#print new_to_tree.shape
 			if self.kdtree_data == None :
+
 				self.kdtree_data = new_to_tree
 			else :
-				self.kdtree_data = numpy.hstack(self.kdtree_data, new_to_tree)
+				#print self.kdtree_data.shape
+				self.kdtree_data = numpy.concatenate((self.kdtree_data, new_to_tree), axis=0)
+			
 
 
 			self.kdtree = KDTree(self.kdtree_data)
 			self.buffer = []
 			self.buffer_count = 0
+			self.buffer_size = self.buffer_size * 2
 
 			#self.buffer_size = self.buffer_size * 2
 
@@ -83,22 +97,29 @@ class Buffered_KD_Tree :
 				closest_point = point
 
 		if self.kdtree != None :
+			
 			kd_distance, qnearI = self.kdtree.query(p)
+			#print "GOT PASSSEEEEEEEEDDDDDDD QUEEEERRRRRYYYYYY"
 			if kd_distance < closest_buffer_dist :
-				return  self.kdtree_data[:,qnearI]
+				return  self.kdtree_data[qnearI,:]
 
 		return closest_point
 
 
 
 def path_from(path, node) :
+	print path
 	return_path = []
 	return_path.append(path[len(path)-1][1])
-	looking_for = path[len(path)-1][0]
+	looking_for = (path[len(path)-1])[0]
+	print looking_for
 	for i in xrange( len(path)-1, -1, -1) :
-		if looking_for == path[i][1] :
-			return_path.append(path[i][1])
-			looking_for == path[i][0]
+		if looking_for == None :
+			break
+
+		if np.array_equal(looking_for, (path[i])[1]) :
+			return_path.append((path[i])[1])
+			looking_for == (path[i])[0]
 
 
 	return return_path
@@ -108,8 +129,8 @@ def simplify_path(path, n) :
 	
 
 	for i in xrange(0, n) :
-		p1 = random.uniform(0,len(path)-1)
-		p2 = random.uniform(0,len(path)-1)
+		p1 = random.randint(0,len(path)-1)
+		p2 = random.randint(0,len(path)-1)
 		while p1 == p2 and len(path) > 2:
 			p2 = random.uniform(0,len(path)-1)
 		if Line_Collision_Check(path[p1], path[p2]) :
@@ -130,7 +151,6 @@ def simplify_path(path, n) :
 
 
 def RRT_Connect_Planner(qinit, qgoal,k) :
-	print sample_cspace()
 
 	Ta = Buffered_KD_Tree(qinit, 20)
 	path_a = []
@@ -140,42 +160,62 @@ def RRT_Connect_Planner(qinit, qgoal,k) :
 	path_b = []
 	path_b.append((None,qgoal))
 
-	delta = .05 #delta step to random point
+	delta = .1 #delta step to random point
 
 	for i in xrange(0,k) : #might change to while
+		print ""
+		print "NEW ITERATION"
 		qrand = sample_cspace()
-		while True : #this will make sure q rand is a valid point, might not be necessary
-			if Point_Collision_Check(qrand) :
-				break
-			qrand = sample_cspace()
+		#while True : #this will make sure q rand is a valid point, might not be necessary
+		#	if Point_Collision_Check(qrand) :
+		#		break
+		#	qrand = sample_cspace()
 
+		print "QRAND"
+		print qrand
+		print "-----"
 		
 		#qrand is a valid point, now let's find the nearest neighbor,qnear
 		qnear = None
-		if i%2 : #work from end
+		if i%2 == 1: #work from end
 			qnear = Tb.Nearest_Neighbor(qrand)
 		else : #work from beginning
 			qnear = Ta.Nearest_Neighbor(qrand)
 
+		print "QNEAR"
+		print qnear
+		print "-----"
+
 		#then take a step toward it to make qnew
 		to_vec = np.linalg.norm(qrand - qnear)
-		qnew = qnear + (to_vec * delta)
 
+		qnew = qnear + ((qrand-qnear)/to_vec * delta)
+		print "QNEW:"
+		print qnew
+		print "-----"
 		#do collision check between qnew and qnear
 		if Line_Collision_Check(qnear,qnew) :
 
 			#valid so add to tree
 
-			if i%2 : #add to end
+			if i%2 == 1 : #add to end
 				Tb.insert(qnew)
 				path_b.append((qnear,qnew))
 				nearest_solution = Ta.Nearest_Neighbor(qnew)
+				print "NEAREST SOLUTION"
+				print nearest_solution
+				print "-----"
+				
 				end_distance = distance.euclidean(qnew, nearest_solution)
+				print "Distance to Solution"
+				print end_distance
 				#check if qnear is close enough to opposite tree
-				if end_distance <= delta and Line_Collision_Check(nearest_solution,qnew) :
+				if end_distance <= .05 and Line_Collision_Check(nearest_solution,qnew) :
 					#the trees can reach each other to create the path
 					atohere = path_from(path_a, nearest_solution).reverse()
 					heretob = path_from(path_b, qnew)
+					if atohere == [] or atohere == None:
+						return heretob
 					return atohere.append(heretob)
 				
 			#do opposite as above for this case
@@ -183,10 +223,17 @@ def RRT_Connect_Planner(qinit, qgoal,k) :
 				Ta.insert(qnew)
 				path_a.append((qnear,qnew))
 				nearest_solution = Tb.Nearest_Neighbor(qnew)
+				print "NEAREST SOLUTION"
+				print nearest_solution
+				print "-----"
 				end_distance = distance.euclidean(qnew, nearest_solution)
-				if end_distance <= delta and Line_Collision_Check(nearest_solution,qnew) :
+				print "Distance to Solution"
+				print end_distance
+				if end_distance <= .05 and Line_Collision_Check(nearest_solution,qnew) :
 					atohere = path_from(path_a,qnew).reverse()
 					heretob = path_from(path_b,nearest_solution)
+					if atohere == [] or atohere == None:
+						return heretob
 					return atohere.append(heretob)
 
 	print "Failed to find path in k steps"
@@ -196,13 +243,17 @@ def RRT_Connect_Planner(qinit, qgoal,k) :
 
 def test() :
 	qi = sample_cspace()
+	print "Looking for a path between: "
+	print qi
 	qf = sample_cspace()
+	print qf
 
 	path = RRT_Connect_Planner(qi, qf, 10000)
 	print path
-
-	path = simplify_path(path, 1000)
-	print path
+	if path != [] :
+		print "Simplifying path"
+		path = simplify_path(path, 1000)
+		print path
 
 if __name__ == '__main__':
     test()
